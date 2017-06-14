c302d43971a9e8940d16214db989b6bce88592be
weirong <weirong@vicancy.com>
2017-06-14 15:50:00 +0200

    testing

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,7 +1,22 @@
 import Ember from 'ember';
-import AjaxService from 'ember-ajax/services/ajax';
+    App = Ember.Application.create({});
 
-export default Ember.Route.extend({
-    host: 'https://dashboard-staging.hrofficelabs.com',
-    contentType: 'application/json; charset=utf-8'
-});
+    App.Token = Ember.Object.extend({
+      token: ""
+    });
+
+    App.IndexRoute = Ember.Route.extend({
+      model: function () {
+        return App.Token.create()
+      },
+      setupController: function (controller, model) {
+        controller.set("model", model);
+      }
+    });
+
+    App.IndexController = Ember.ObjectController.extend({
+      submitAction: function () {
+        // here you could perform your actions like persisting to the server or so
+        alert("now we can submit the model:" + this.get("model"));
+      }
+    });
